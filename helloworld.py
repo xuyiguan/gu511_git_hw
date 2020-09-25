6ff58383852094ba1439acd82566c92768335c6e
guanxuyi <guanxuyi@bu.edu>
2020-09-25 23:48:55 +0800

    helloworld:initial commit

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def hello_world():
+    print("hellor world!")
+    now = datetime.datetime.now()
+    print(f'it is {now}')
+    
+if __name__ == "__main__":
+    hello_world()
+
